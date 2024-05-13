4ecfe91ffa9bd7af3a231437cdf9d39e8b6f1651
Xudong Shi <iamshixudong@gmail.com>
2024-05-13 21:39:58 +0200

    restore workable sor program

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
-mod sor;
+use std::env;
+
+mod sor_sendrecv;
 mod sor_rma;
 mod sor_seq;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let mpi_type: &str = &args[1];
+    let size: usize = args[2].parse().expect("Failed to parse args[2] as u32");
+    match mpi_type {
+        "rma" => sor_rma::sor(size),
+        "norm" => sor_sendrecv::sor(size),
+        _ => println!("Invalid argument, run either ping pong | sor_source_data, rma | norm"),
+    }
 }

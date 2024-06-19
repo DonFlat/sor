fdc96adfa88dd9a2d2fad8ea42ea2a1b92e31def
Xudong Shi <iamshixudong@gmail.com>
2024-06-19 06:33:03 +0200

    commits

--- increasing_prob_size_test_runner.py
+++ increasing_prob_size_test_runner.py
@@ -6,7 +6,7 @@ import subprocess
 
 def run_command(command):
     try:
-        subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        subprocess.run(command, shell=True, check=True)
     except subprocess.CalledProcessError as e:
         print(f"An error occurred: {e.stderr}")
 
